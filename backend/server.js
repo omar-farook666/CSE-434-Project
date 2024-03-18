299f9d5d640a053cc312ebec90483f13d45c21d5
MrMeatbox <u1804125student.cuet.ac.bd>
2024-03-18 22:53:00 +0600

    added backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+  
+
+/*
+ * Name: Hotel Room Booking System ~ Backed
+ * Description: Build an Hotel Room Booking System using node.js, express.js application from the scratch
+ * Author: Md. Samiur Rahman (Mukul)
+ * Last Modified: 26/02/2023
+ * Version: v0.0.1
+ *
+ */
+
+// imports modules & dependencies
+const app = require('./src/app');
+const logger = require('./src/middleware/winston.logger');
+
+// app listens to .env defined port
+app.listen(process.env.APP_PORT, () => {
+  logger.info(`App server running on: ${process.env.APP_BASE_URL}`);
+});
